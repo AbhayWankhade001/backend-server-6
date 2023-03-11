ef6f486ebe38f3b6738207a58559299649e0071d
AbhayWankhade001 <110882315+AbhayWankhade001@users.noreply.github.com>
2023-03-11 20:14:51 +0530

    Update conn.js

--- database/conn.js
+++ database/conn.js
@@ -4,7 +4,9 @@ import ENV from '../router/config.js';
 async function connect(){
     const db = await mongoose.connect(ENV.ATLAS_URI, {
         useNewUrlParser: true,
-        useUnifiedTopology: true
+  useUnifiedTopology: true,
+  useFindAndModify: false,
+  useCreateIndex: true,
     });
     console.log("Database Connected");
     return db;
